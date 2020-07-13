cf5cc5c2d68095693aeccdf0be23facbc0218aa4
MuhammedUlviOzkaya <68199586+MuhammedUlviOzkaya@users.noreply.github.com>
2020-07-13 19:16:21 +0300

    Delete Calculator.py

--- venv/Calculator.py
+++
@@ -1,8 +0,0 @@
-num1 = float(input("First number :"))
-num2 = float(input("Second number:"))
-
-print("\n")
-print("Toplama sonucu =", num1+num2)
-print("Çıkarma sonucu =", num1-num2)
-print("Çarpma sonucu  =", num1*num2)
-print("Bölme sonucu   =", num1/num2)
